ed3b06c76d3dda0470218ef73dfd97273b3644c3
Frédéric Thériault <fred.theriault@gmail.com>
2022-08-24 10:30:19 -0400

    Done with loops

--- PHP - La base/Notes/02 - boucles.php
+++ PHP - La base/Notes/02 - boucles.php
@@ -6,6 +6,32 @@
 	</head>
 	<body>
 		<h1>Champs radio</h1>
+		<?php
+			for ($i = 0; $i < 10; $i++) {
+				?>
+				<div>
+					<input type="radio" name="a" value="<?= $i ?>"> <?= $i ?>
+				</div>
+				<?php
+				// phpr = php tags, reversed
+			}
+
+			$tab = array();
+			$tab = []; // même chose
+
+			$tab[] = "Hello"; // push!
+			$tab[] = "World";
+
+			var_dump($tab);
+
+			foreach ($tab as $element) {
+				?>
+				<div>
+					<?= $element ?>
+				</div>
+				<?php
+			}
+		?>
 	</body>
 </html>
 
