fa420bc9e8f53430ffd9a18019acf6d925184399
Frédéric Thériault <fred.theriault@gmail.com>
2022-08-24 11:23:38 -0400

    Done with functoins

--- PHP - La base/Notes/05 - fonctions.php
+++ PHP - La base/Notes/05 - fonctions.php
@@ -1,3 +1,9 @@
+<?php
+	function hasher($mot1, $mot2 = "") {
+
+		return password_hash($mot1 . $mot2, PASSWORD_DEFAULT);
+	}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -6,6 +12,12 @@
 	</head>
 	<body>
 		<h1>Création d'un hash</h1>
+		<div>
+			<?= hasher("john") ?>
+		</div>
+		<div>
+			<?= hasher("john") ?>
+		</div>
 	</body>
 </html>
 
